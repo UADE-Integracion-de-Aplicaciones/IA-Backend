e8aa64512a9702198e84003c2cb3cdcb613c26a4
bizk <santiago.yanzon1999@gmail.com>
2020-11-06 12:44:10 -0300

    Merge changes

---
+++ src/routes/client.routes.js
@@ -0,0 +1,9 @@
+'use strict';
+module.exports = (app) => {
+    var clientController = require('../controllers/client');
+
+    app.get('/client', (req,res) => clientController.getClient(req, res)); 
+    app.delete('/client', (req,res) => clientController.delete(req, res)); 
+    app.post('/client', (req,res) => clientController.create(req, res)); 
+    app.put('/client', (req,res) => clientController.modify(req, res)); 
+}
