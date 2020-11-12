2075c0646f9e8a8409c24dc5d4e8f7c7ea6b412f
Alexander Cabezas <alexcabezas1@gmail.com>
2020-11-12 13:42:53 -0300

    Registrar usuario

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,28 +1,21 @@
-'use strict';
+"use strict";
 module.exports = (app) => {
-    var userController = require('../controllers/user.controller.');
+  var userController = require("../controllers/user.controller");
 
-    app.post('/login', (req,res) => {
-        // #swagger.tags = ['Usuario']
-        // #swagger.description = 'Endpoint loggin de un usuario.'
-        // #swagger.parameters['nombre_usuario'] = { description: 'nombre de usuario.'}
-        // #swagger.parameters['clave'] = { description: 'clave de usuario.', type: 'string' }
-        userController.login(req, res)}
-    ); 
-        
-    app.post('/register', (req, res) => {
-        // #swagger.tags = ['Usuario']
-        // #swagger.description = 'Endpoint para registrar un usuario.'
-        // #swagger.parameters['nombre_usuario'] = { description: 'nombre de usuario.'}
-        // #swagger.parameters['clave'] = { description: 'clave de usuario.', type: 'string' }
-        // #swagger.parameters['rol_id'] = { description: 'id del rol del usuario.', type: 'string' }
-        userController.register(req,res)
-    });
+  app.post("/login", (req, res) => {
+    // #swagger.tags = ['Usuario']
+    // #swagger.description = 'Endpoint loggin de un usuario.'
+    // #swagger.parameters['nombre_usuario'] = { description: 'nombre de usuario.'}
+    // #swagger.parameters['clave'] = { description: 'clave de usuario.', type: 'string' }
+    userController.login(req, res);
+  });
 
-    app.get('/usuario', (req,res) => {
-        // #swagger.tags = ['Usuario']
-        // #swagger.description = 'Verifica si el usuario existe.'
-        // #swagger.parameters['id'] = { description: 'id de usuario.'}
-        userController.login(req, res)}
-    ); 
-}
+  app.post("/register", (req, res) => {
+    // #swagger.tags = ['Usuario']
+    // #swagger.description = 'Endpoint para registrar un usuario.'
+    // #swagger.parameters['nombre_usuario'] = { description: 'nombre de usuario.'}
+    // #swagger.parameters['clave'] = { description: 'clave de usuario.', type: 'string' }
+    // #swagger.parameters['rol_id'] = { description: 'id del rol del usuario.', type: 'string' }
+    userController.register(req, res);
+  });
+};
