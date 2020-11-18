cc0c05d41959f892197723905ab6a83d1dafb2c6
Alexander Cabezas <alexcabezas1@gmail.com>
2020-11-17 23:27:40 -0300

    Ajustes obtener facturas por codigo de pago electronico

--- src/controllers/facturas.controller.js
+++ src/controllers/facturas.controller.js
@@ -1,39 +1,42 @@
-const facturasDao = require('../daos/facturas.dao');
+const {
+  cargarFacturas,
+  obtenerFacturasPorCodigoPagoElectronico,
+} = require("../daos/facturas.dao");
+
 module.exports = {
- 
-    async cargar(req,res){
-        const { path } = req.file;
-        const { numero_cuenta } = req.body;
-        var columns = true; 
-        try {
-            await facturasDao.cargarFacturas(path, numero_cuenta, columns);
-            res.status(200).json({ mensaje: "Facturas cargadas" });
-        }catch (err) {
-            console.log(err)
-            res.status(500).json({ mensaje: err });
-        }
-    },
-     
-    async getFacturas(req, res) {
-        const { codigo_pago_electronico } = req.body;
-        if (!codigo_pago_electronico || !req.body) {
-            res.status(301).send("No se encuentran campos.");
-            return ;
-        }
-        try {
-            await facturasDao.getFacturaByCodigoPagoElectronico( codigo_pago_electronico )
-                .then(factura => {
-                    if (!factura) {
-                        res.status(300).send("No se encontraron facturas");
-                        return ;
-                    }
-                    res.status(200).send(factura);
-                }).catch(err => {
-                    console.log(err)
-                    res.status(400).send("Error al buscar facturas, paso algo")
-                })
-        } catch (err) {
-            res.status(500).json({ mensaje: "Error en el servidor al buscar factura" });
-        }
+  async cargar(req, res) {
+    const { path } = req.file;
+    const { numero_cuenta } = req.body;
+    var columns = true;
+
+    try {
+      await cargarFacturas(path, numero_cuenta, columns);
+      return res.status(200).json({ mensaje: "Facturas cargadas" });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ mensaje: err });
     }
-}
+  },
+
+  async obtenerFacturas(req, res) {
+    const { params } = req;
+    const { codigo_pago_electronico } = params;
+
+    try {
+      const facturas = await obtenerFacturasPorCodigoPagoElectronico(
+        codigo_pago_electronico
+      );
+
+      const respuesta = facturas.map((factura) => ({
+        numero_factura: factura.get("numero_factura"),
+        importe: factura.get("importe"),
+        fecha_vencimiento: factura.get("fecha_vencimiento"),
+        fecha_pagado: factura.get("fecha_pagado"),
+      }));
+      return res.status(200).json({ facturas: respuesta });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ error });
+    }
+  },
+};
