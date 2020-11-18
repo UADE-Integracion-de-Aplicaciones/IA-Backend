160dacf03c7de9bfc9f11b02fcac34dd3b18a228
bizk <santiago.yanzon1999@gmail.com>
2020-11-18 12:34:26 -0300

    removed unused line

--- src/app.js
+++ src/app.js
@@ -42,7 +42,6 @@ require("./routes/clientes.routes")(protectedRouter);
 require("./routes/transacciones.routes")(protectedRouter);
 require("./routes/cuentas.routes")(protectedRouter);
 require("./routes/facturas.routes")(app);
-require("./routes/bancos.routes")(app);
 require("./routes/transacciones.bancos.routes")(app);
 
 module.exports = app;
